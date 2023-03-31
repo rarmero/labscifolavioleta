8c141e898da3ad66e6a9c1a412f419e865c04023
rafa <rarmero@hotmail.com>
2023-03-31 09:19:06 +0200

    springbootrestapi

---
+++ src/springbootRestApi/src/main/java/com/example/restapi/Repository/ReservationRepository.java
@@ -0,0 +1,45 @@
+
+package com.example.restapi.Repository;
+
+import org.springframework.stereotype.Repository;
+import org.electrolinera.manager.ReservationManager;
+import org.electrolinera.model.Reservation;
+
+import java.util.List;
+
+
+@Repository
+public class ReservationRepository {
+    private ReservationManager reservationManager= new ReservationManager();
+
+    public void add(Reservation reservation){
+        reservationManager.addReservation(reservation);
+    }
+
+    public List<Reservation> getAllReservations(){
+        return  reservationManager.reservationList;
+    }
+
+    public Reservation getReservationsByIdUser(long idUser){
+        return reservationManager.getReservationById(idUser);
+    }
+
+    public void reservationDelete(Reservation reservation){
+        reservationManager.deleteReservation(reservation);
+    }
+
+    public void reservationDeleteById(int idReservation){
+        reservationManager.deleteReservationById(idReservation);
+    }
+
+    public void reservationUpdate(int index,Reservation reservation){
+        reservationManager.updateReservation(index,reservation);
+    }
+
+    public Reservation reservationDetail(int index){
+        return  reservationManager.getReservation(index);
+    }
+    public Reservation getReservationById(int index){
+        return  reservationManager.getReservation(index);
+    }
+}
